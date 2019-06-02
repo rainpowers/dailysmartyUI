125b8e7fc848c798198b52bcc80b61e2c8f1d1fa
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-06-01 20:05:16 -0700

    created recent posts and searchbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+import Logo from './logo';
+import SearchBar from './searchBar';
+import RecentPosts from './recentPosts';
 export default class App extends Component {
   render() {
     return (
       <div>
-        logo
+        <Logo/>
+        <SearchBar/>
+        <RecentPosts/>
       </div>
     );
   }
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Logo from './logo';
+
+
+
 class Logo extends Component {
   render() {
     return (
