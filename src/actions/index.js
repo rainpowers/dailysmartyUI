eedd34c3b694de44b1d2ea52807a812d8bf71b20
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-06-03 23:57:17 -0700

    setup redux thunk and connecting to it

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export function fetchRecentPosts(){
+    return function(dispatch){
+        //perform our requests from here
+        console.log('hello')
+    }
+}
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 class RecentPosts extends Component {
-  render() {
-    return (
-      <div className= "recent-posts">
-          <div className="recent-posts-wrapper">
-              <div className="recent-posts__heading">Recent Posts</div>
-              <ul className= "recent-posts__posts">
-                <li> recent posts 0</li>
-                <li> recent posts 1</li>
-                <li> recent posts 2</li>                  
-              </ul>
-          </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
+                        <li>recent post 0</li>
+                        <li>recent post 1</li>
+                        <li>recent post 2</li>
+                    </ul>
+                </div>
+
+            </div>
+        )
+    }
 }
 
-export default RecentPosts;
+export default connect(null,RecentPosts)(SearchBar);
