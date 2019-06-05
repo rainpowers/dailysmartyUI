f7007b2df697f0df26c891e7e0f19293410e4aa2
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-06-04 23:10:52 -0700

    setup redux devtools and fixed reducer for set_recent_posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
+
 import { reducer as form } from 'redux-form';
+
+import posts from './postsReducer';
+
+
 const rootReducer = combineReducers({
-  form
+  form,
+  posts
 });
 
 export default rootReducer;
