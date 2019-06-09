2d64cf96528629f24294b575a1ffefa03fc07744
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-06-09 11:51:39 -0700

    added react-animate-height npm package and used it to add hover animations to the post links

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import AnimateHeight from 'react-animate-height';
 
 class Post extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            height: 0
+        }
+    }
 
     renderTopics() {
         let topics = this.props.associated_topics.map((topic, index) => {
@@ -18,7 +27,7 @@ class Post extends Component {
                         <a href={post_link.link_url}>Useful Link #{index + 1}</a>
                     </div>
                 </div>
-           )
+            )
         })
         return links;
     }
@@ -42,17 +51,25 @@ class Post extends Component {
                         {this.renderTopics()}
                     </div>
                     <div className="result-post__title">
-                        {this.props.title}
+                        <a href={this.props.url_for_post}
+                           onMouseEnter={() => this.setState({ height: 70 })}
+                           onMouseLeave={() => this.setState({ height: 0 })}
+                        >
+                        {this.props.title}</a>
                     </div>
-                    <div className="result-post__links">
-                        {this.renderLinks()}
-                    </div>
+                    <AnimateHeight
+                        duration={500}
+                        height={this.state.height}
+                    >
+                        <div className="result-post__links">
+                            {this.renderLinks()}
+                        </div>
+                    </AnimateHeight>
                 </li>
             )
         }
 
-
     }
 }
 
-export default Post; 
+export default Post;
